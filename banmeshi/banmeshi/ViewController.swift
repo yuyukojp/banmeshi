b0202268f89c9387d2dd37a0c7ac25b142d3526e
S <sunchengxin@fenrir-tec.com>
2021-12-28 15:17:32 +0800

    base作成

---
+++ banmeshi/banmeshi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banmeshi
+//
+//  Created by 金斗石 on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
