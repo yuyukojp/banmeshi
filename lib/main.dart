5d539d81105f12838a969b7d75518220182806ed
S <sunchengxin@fenrir-tec.com>
2022-04-15 11:35:28 +0800

    Flutterの各種類のボタン

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'タイトル',
+      title: 'Welcome to Flutter',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Appbarタイトル'),
+          title: new Text('Welcome to Flutter'),
         ),
-        body: new Center(
-          child: new Text(
-            'ハローワールド',
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('RaisedButton'),
                 color: Colors.blue,
-                fontSize: 32,
-                decoration: TextDecoration.underline),
+                textColor: Colors.red,
+                onPressed: () => {print('Tap button')},
+              ),
+              FlatButton(
+                child: Text('FlatButton'),
+                textColor: Colors.red,
+                onPressed: () => {},
+              ),
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () => {},
+              ),
+              OutlineButton(
+                child: Text('OutlineButton'),
+                color: Colors.blue,
+                textColor: Colors.red,
+                onPressed: () => {},
+              ),
+            ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => {},
+          tooltip: '你点击的是FloatingActionButton',
         ),
       ),
     );
