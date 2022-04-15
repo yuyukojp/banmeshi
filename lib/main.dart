de9b6126f30bbf5856176f0d464ad1b0cbafec71
S <sunchengxin@fenrir-tec.com>
2022-04-15 10:52:05 +0800

    Base

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'タイトル',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Appbarタイトル'),
+        ),
+        body: new Center(
+          child: new Text('ハローワールド'),
+        ),
+      ),
+    );
+  }
+}
