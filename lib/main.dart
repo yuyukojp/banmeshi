252d68a85f7faaeaaf6ea56ce964f4a4bd4dc8f2
S <sunchengxin@fenrir-tec.com>
2022-04-19 09:26:42 +0800

    ListWheelScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -3,77 +3,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  List<String> list = [
+    'https://cdn.pixabay.com/photo/2017/09/11/20/48/branches-2740419_960_720.png',
+    'https://cdn.pixabay.com/photo/2017/09/11/20/48/branches-2740419_960_720.png'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'タイトル',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
-      //MARK: - ScaffoldのAppナビゲーションバー
       home: new Scaffold(
         appBar: new AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.add_to_photos),
-            onPressed: () => {},
-          ),
           title: new Text('AppBarタイトル'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              tooltip: '追加',
-              onPressed: () => {},
-            ),
-            IconButton(
-              icon: Icon(Icons.delete),
-              tooltip: '削除',
-              onPressed: () => {},
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: '検索',
-              onPressed: () => {},
-            ),
-          ],
         ),
-        body: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround, //(1)
-          crossAxisAlignment: CrossAxisAlignment.stretch, //(2)
-          mainAxisSize: MainAxisSize.min, //(3)
-          textDirection: TextDirection.rtl, //(4)
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.yellow,
-              alignment: Alignment.center,
-              child: Text(
-                '1',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.deepOrange,
-              alignment: Alignment.center,
-              child: Text(
-                '2',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.green,
-              alignment: Alignment.center,
-              child: Text(
-                '3',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-          ],
+        body: RotatedBox(
+          quarterTurns: -1,
+          child: ListWheelScrollView(
+            offAxisFraction: -0.5,
+            diameterRatio: 1.4,
+            itemExtent: 200,
+            children: [
+              for (var i in List.generate(100, (i) => i))
+                RotatedBox(
+                  quarterTurns: 1,
+                  child: Container(
+                    width: 100.0,
+                    height: 50.0,
+                    color: Colors.blue[100],
+                    child: Center(
+                      child: Text(
+                        "Index: $i",
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
